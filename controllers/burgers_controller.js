7037ee37fe35deada2fa6eec779a95a0df460c7e
Giuliano Serawop <gserawop13@gmail.com>
2021-03-27 09:05:44 -0600

    updating handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,27 @@ const express = require('express')
 const burger = require('../models/burger')
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    const burgerObj = {
+      burger: data,
+    };
+    console.log(burgerObj);
+    res.render("index", burgerObj);
+  });
+});
+
+router.post("/api/burger", (req, res) => {
+  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
+const app = express();
+const PORT = 3000;
 
+app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+const exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
 
 
